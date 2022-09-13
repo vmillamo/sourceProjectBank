d5ba9b306e142bff6958f15c5b6e0739e25752a2
vmillamo <vmillamo@host.docker.internal>
2022-09-13 18:12:12 -0500

    commit Proyecto Final

---
+++ prjBankAccountManagement/src/main/java/pe/com/restapibank/consumer/KafkaStringConsumer.java
@@ -0,0 +1,18 @@
+package pe.com.restapibank.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaStringConsumer {
+
+    Logger logger = LoggerFactory.getLogger(KafkaStringConsumer.class);
+
+    @KafkaListener(topics = "TOPIC-DEMO" , groupId = "group_id")
+    public void consume(String message) {
+        logger.info("Consuming Message {}", message);
+    }
+
+}
---
+++ prjBankCustomerManagement/src/main/java/pe/com/restapibank/config/KafkaStringConfig.java
@@ -0,0 +1,31 @@
+package pe.com.restapibank.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import com.fasterxml.jackson.databind.ser.std.StringSerializer;
+
+@Configuration
+public class KafkaStringConfig {
+
+    public ProducerFactory<String, String> producerFactory() {
+        Map<String, Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+    @Bean(name = "kafkaStringTemplate")
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+}
